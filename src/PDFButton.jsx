445bf08184830d54ff815d70592d8526f2009fea
Agustina <agustina.frias12@gmail.com>
2025-02-26 08:46:30 -0300

    agregar funcionalidad para generar PDF con título y logo

--- src/PDFButton.jsx
+++ src/PDFButton.jsx
@@ -1,16 +1,35 @@
 import { jsPDF } from "jspdf";
 import "./App.css"; // Importamos los estilos
+import logoSanJuan from "./assets/logoSanJuan.png"; // Asegúrate de que la ruta sea correcta
 
 export default function PDFButton() {
   const generarPDF = () => {
     // Crear una nueva instancia de jsPDF
     const doc = new jsPDF();
 
-    // Agregar texto al PDF en la posición (x=20, y=20)
-    doc.text("Este es un PDF generado en React.", 20, 20);
+    const lineas = ["prueba linea1", "prueba linea2", "prueba linea3"];
 
-    // Guardar y descargar el archivo con el nombre "documento.pdf"
-    doc.save("docReact.pdf");
+    // Configurar el título en negrita y con una letra más grande
+    doc.setFont("helvetica", "bold");
+    doc.setFontSize(16);
+    doc.text("Titulo: Generador de PDF en React", 40, 10);
+
+    // Agregar el logo de San Juan a la izquierda del título
+    const img = new Image();
+    img.src = logoSanJuan;
+    img.onload = () => {
+      doc.addImage(img, "PNG", 10, 5, 25, 10); // Ajusta las coordenadas y el tamaño según sea necesario
+
+      // Configurar el texto normal
+      doc.setFont("helvetica", "normal");
+      doc.setFontSize(12);
+      lineas.map((linea, index) => {
+        doc.text(linea, 20, 30 + (index * 10)); // Ajusta la posición vertical para dejar espacio al logo
+      });
+
+      // Guardar y descargar el archivo con el nombre "documento.pdf"
+      doc.save("docReact.pdf");
+    };
   };
 
   return (
